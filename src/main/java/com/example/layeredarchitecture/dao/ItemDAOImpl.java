8fb4640786ff87795520c4dd738d149be78b03d1
pubudiniashani <pubudiniashani2000@gmail.com>
2023-12-15 14:10:39 +0530

    Initial commit

---
+++ src/main/java/com/example/layeredarchitecture/dao/ItemDAOImpl.java
@@ -0,0 +1,54 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.db.DBConnection;
+import com.example.layeredarchitecture.model.ItemDTO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class ItemDAOImpl {
+
+    public ArrayList<ItemDTO> getAllItems() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Item");
+
+        ArrayList<ItemDTO> getAllItem = new ArrayList<>();
+
+        while (rst.next()){
+            ItemDTO itemDTO = new ItemDTO(rst.getString("code"),
+                    rst.getString("description"),
+                    rst.getBigDecimal("unitPrice"), rst.getInt("qtyOnHand"));
+
+            getAllItems().add(itemDTO);
+        }
+        return getAllItem;
+
+
+    }
+
+    public boolean saveItem(ItemDTO dto) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)");
+
+        pstm.setString(1, dto.getCode());
+        pstm.setString(2, dto.getDescription());
+        pstm.setBigDecimal(3, dto.getUnitPrice());
+        pstm.setInt(4, dto.getQtyOnHand());
+
+      return pstm.executeUpdate() > 0;
+    }
+
+    public void updateItem(ItemDTO dto) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)");
+        pstm.setString(1, dto.getCode() );
+        pstm.setString(2, dto.getDescription());
+        pstm.setBigDecimal(3, dto.getUnitPrice());
+        pstm.setInt(4, dto.getQtyOnHand());
+
+        pstm.executeUpdate();
+    }
+
+
+}
